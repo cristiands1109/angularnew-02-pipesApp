e72c50e02a82c84707d13bb1abb541b12ab233eb
Christian Duarte <cristiands1109@gmail.com>
2021-04-01 20:47:11 -0400

    Fin de la Seccion 11

---
+++ src/app/ventas/pipes/mayusculas.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    name: 'mayusculas'
+})
+export class MayusculasPipe implements PipeTransform {
+
+
+    transform(valor: string, enMayusculas: boolean = true): string {
+
+        // if(enMayusculas == true) {
+
+        //     return valor.toLocaleUpperCase();
+        // } else {
+        //     return valor.toLocaleLowerCase();
+        // }
+
+        // forma ternaria
+        return (enMayusculas)
+            ? valor.toLocaleUpperCase()
+            : valor.toLocaleLowerCase()
+    }
+
+
+}
---
+++ src/app/ventas/pipes/vuela.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+
+
+@Pipe({
+    name: 'vuelapipe'
+})
+export class VuelaPipe implements PipeTransform {
+    transform(valor: boolean): string {
+        return (valor) ? 'vuela' : 'no vuela'
+    }
+}
